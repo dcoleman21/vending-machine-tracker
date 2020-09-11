70e25cfc31a3bb5ef7fcb9c765efb17efa30f922
Dani Coleman <danicoleman00@gmail.com>
2020-09-10 19:33:09 -0600

    Complete User story 1 & 2

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -9,4 +9,41 @@ RSpec.describe 'When a user visits a vending machine show page', type: :feature 
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
   end
+
+  it "sees the name of all of the snacks and their price" do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+
+    doritos = dons.snacks.create(name: "Doritos", price: 1.75)
+    donuts = dons.snacks.create(name: "Donuts", price: 2.00)
+
+    MachineSnack.create(machine_id: dons.id, snack_id: doritos.id)
+
+    visit machine_path(dons)
+
+    expect(page).to have_content("#{dons.location} Vending Machine")
+    expect(page).to have_content(doritos.name)
+    expect(page).to have_content(doritos.price)
+    expect(page).to have_content(donuts.name)
+    expect(page).to have_content(donuts.price)
+  end
+
+  it "sees an average price for all of the sancks in that machine" do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+
+    doritos = dons.snacks.create(name: "Doritos", price: 1.75)
+    donuts = dons.snacks.create(name: "Donuts", price: 2.00)
+
+    MachineSnack.create(machine_id: dons.id, snack_id: doritos.id)
+
+    visit machine_path(dons)
+
+    expect(page).to have_content("#{dons.location} Vending Machine")
+    expect(page).to have_content(doritos.name)
+    expect(page).to have_content(doritos.price)
+    expect(page).to have_content(donuts.name)
+    expect(page).to have_content(donuts.price)
+    expect(page).to have_content(dons.average_snack_price)
+  end
 end
--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -5,4 +5,24 @@ RSpec.describe Machine, type: :model do
     it { should validate_presence_of :location }
     it { should belong_to :owner }
   end
+
+  describe 'relationships' do
+    it { should have_many :machine_snacks }
+    it { should have_many(:snacks).through(:machine_snacks) }
+  end
+
+  describe "#average_snack_price" do
+    it "returns average price of its snacks" do
+      owner = Owner.create(name: "Sam's Snacks")
+      dons  = owner.machines.create(location: "Don's Mixed Drinks")
+
+      doritos = dons.snacks.create(name: "Doritos", price: 1.00)
+      donuts = dons.snacks.create(name: "Donuts", price: 2.00)
+
+      dons.snacks << doritos
+      dons.snacks << donuts
+
+      expect(dons.average_snack_price).to eq(1.50)
+    end
+  end
 end
